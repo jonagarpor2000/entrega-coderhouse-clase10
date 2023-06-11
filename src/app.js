2d42b3f414cff756c9d8ac0a840ccd3e2735d904
jonagarpor2000 <jonathan.porley15@gmail.com>
2023-06-11 16:03:08 -0300

    first commit (w.i.p)

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import { Server } from 'socket.io';
+import productRouter from './routers/product.router.js';
+import * as path from 'path'
+import __dirname from './utils.js';
+
+
+const app = express()
+const httpServer = app.listen(8080, () => console.log('Srv Up!'))
+//const io = new Server(httpServer)
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
+
+app.engine('handlebars', handlebars.engine())
+app.set('view engine', 'handlebars')
+app.set('views', path.resolve(__dirname+'/views'))
+app.use(express.static(path.resolve(__dirname+'/public')))
+
+app.use('/', (req,res)=> res.render('layouts/main'))
+
+//const messages = []
+
+/*io.on('connection', socket => {
+    socket.broadcast.emit('alerta')
+    socket.emit('logs', messages)
+    socket.on('message', data => {
+        messages.push(data)
+        io.emit('logs', messages)
+    })
+})*/
+
